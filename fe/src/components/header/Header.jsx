17f8fdfc5c8dae632e6046149a1bf2916ef4dcfa
DucManh <manhdb7501@gmail.com>
2024-01-23 16:36:49 +0700

    test commit

--- fe/src/components/header/Header.jsx
+++ fe/src/components/header/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-	return <div>Header</div>;
+  return (
+    <div className="RC-container">
+      <div className="wrapper">Manh</div>
+    </div>
+  );
 };
 
 export default Header;
